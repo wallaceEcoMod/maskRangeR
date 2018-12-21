9216ddd5dacce1fc5e891e7dcaac00c81ac3bd64
ndimhypervol <jamie.m.kass@gmail.com>
2018-12-21 11:24:05 -0500

    updated docs/comments for svm funcs

--- R/svm.R
+++ R/svm.R
@@ -1,33 +1,29 @@
-#' Classify species ranges based on occurrence coordinates and continuous values from an SDM
+#' Classify species ranges based on occurrence coordinates and SDM scores.
 #'
 #' \code{rangeSVM()} returns a tuned support vector machine (SVM) model that predicts 
-#' species identity based on predictors that are either (1) solely spatial, (2) based 
-#' on occurrence coordinates, or (3) a combination of spatial and environmental, based 
-#' on both occurrence coordinates and environmental suitability values. Suitability 
-#' values can be predicted with species distribution models (SDMs; a.k.a. ecological 
-#' niche models).
+#' species identity based on predictors that are solely spatial, based on occurrence coordinates,
+#' or a combination of spatial and environmental, based on both occurrence coordinates and 
+#' environmental suitability values. Suitability values can be predicted with species distribution
+#' models (SDMs; a.k.a. ecological niche models).
 #'
 #' @param xy1 Matrix or data frame of occurrence coordinates for species 1.
 #' @param xy2 Matrix or data frame of occurrence coordinates for species 2.
-#' @param sdm Raster or RasterStack representing environmental suitability (can be predictions from SDMs).
-#' Default is NULL.
+#' @param sdm Raster or RasterStack representing environmental suitability (can be predictions from SDMs). 
+#' These must have the same extent as both species' occurrence points. Default is NULL.
 #' @param nrep Numeric for number of SVM tuning iterations. Default is 100.
 #' @param weight Boolean. If TRUE, the species with less occurrence records is weighted higher in the SVM.
 #' Default is FALSE.
 #' @return The tuned SVM model.
-#' @details The tuning operation uses \code{tune.svm()} from the e1071 package, 
-#' which performs 10-fold cross validation and selects the best model based on 
-#' classification error. Ranges of the cost and gamma parameters are explored in 
-#' the tuning exercise. The tuning function is iterated \code{nrep} times, and the
-#' parameter combination used most frequently across all iterations is used to 
-#' build a final SVM model.
+#' @details The tuning operation uses \code{tune.svm()} from the e1071 package, which performs 10-fold
+#' cross validation and selects the best model based on classification error. Ranges of the cost and gamma
+#' parameters are explored in the tuning exercise. The tuning function is iterated \code{nrep} times, and the
+#' parameter combination used most frequently across all iterations is used to build a final SVM model.
 #' 
-#' When \code{sdm = NULL}, the SVM is purely spatial, based only on the occurrence 
-#' coordinates of each species. Otherwise, the SVM is fit with both a spatial 
-#' predictor and any additional ones added as rasters. These extra predictors can be 
-#' based on predictions from a species distribution model (SDM; a.k.a. ecological 
-#' niche model), and in this case would represent environmental or climatic suitability, 
-#' depending on the variables used in the SDM.
+#' When \code{sdm = NULL}, the SVM is purely spatial, based only on the occurrence coordinates of
+#' each species. Otherwise, the SVM is fit with both a spatial predictor and any additional ones added as
+#' rasters. These extra predictors can be based on predictions from a species distribution model 
+#' (SDM; a.k.a. ecological niche model), and in this case would represent environmental or climatic
+#' suitability, depending on the variables used in the SDM.
 #' 
 #' 
 #' @examples
@@ -35,28 +31,38 @@
 #' # tune SVMs on coordinates only (spatial)
 #' rangeSVM(xy1, xy2)
 #' 
-#' # tune SVMs on coordinates and SDM prediction rasters (hybrid)
+#' # tune SVMs on coordinates and SDM prediction rasters (spatial + environmental)
 #' rangeSVM(xy1, xy2, raster::stack(sdm1, sdm2))
 #' }
 #' @export
+#' 
+#' 
+
+# to clarify:
+# 1. specifics on optimality criterion (weights false positives and false negatives equally?)
+# 2. nothing to break tie (how do we choose when there are ties?)
+# 3. could easily allow users to change k
+# 4. sensitivity to relative number of points near border (and assumes that sampling bias was addressed?)
+# 5. which params are varied and what they mean
+# 6. convey to Cory to make generalizable to more than 2 species
 
 rangeSVM <- function(xy1, xy2, sdm = NULL, nrep = 100, weight = FALSE) {
   # define class weights
-  if(weight == TRUE) {
-    if(nrow(xy1) != nrow(xy2)) {
-      if(nrow(xy1) > nrow(xy2)) {
-        cw <- c("0" = 1, "1" = nrow(xy1)/nrow(xy2))
-      } else {
-        cw <- c("0" = nrow(xy2)/nrow(xy1), "1" = 1)
-      }  
-    }  
-  } else {
-    cw <- c("0" = 1, "1" = 1)
-  }
+  # if(weight == TRUE) {
+  #   if(nrow(xy1) != nrow(xy2)) {
+  #     if(nrow(xy1) > nrow(xy2)) {
+  #       cw <- c("0" = 1, "1" = nrow(xy1)/nrow(xy2))
+  #     } else {
+  #       cw <- c("0" = nrow(xy2)/nrow(xy1), "1" = 1)
+  #     }  
+  #   }  
+  # } else {
+  #   cw <- c("0" = 1, "1" = 1)
+  # }
   
   # bind both coordinate matrices
   xy <- rbind(xy1, xy2)
-  # if sdm prediction raster input, add the values to the matrix 
+  # if using sdm prediction rasters as extra predictor variable, add the values to the matrix 
   if(!is.null(sdm)) {
     sdm.vals <- raster::extract(sdm, xy)
     xy <- cbind(xy, sdm = sdm.vals)
@@ -77,12 +83,17 @@ rangeSVM <- function(xy1, xy2, sdm = NULL, nrep = 100, weight = FALSE) {
   # year={2005},
   # publisher={Department of Statistics and Mathematics, WU Vienna University of Economics and Business}
   params_best <- list()
+  # performance_best <- list()
   gamma_best <- numeric(nrep)
   C_best <- numeric(nrep)
+  
   for(i in 1:nrep) {
     m.tune <- e1071::tune.svm(sp ~ ., data = xy, gamma = gamma_range, cost = C_range, class.weights = cw)  
+    # print(sum(as.numeric(as.character(predict(m.tune$best.model, aus.xy)))))
+    # print(sum(as.numeric(as.character(predict(m.tune$best.model, tel.xy)))))
     # get optimal parameter values
     params_best[[i]] <- m.tune$best.parameters
+    # performance_best[[i]] <- m.tune$best.performance
     # gamma_best[i] <- m.tune$best.parameters[[1]]
     # C_best[i] <- m.tune$best.parameters[[2]]
     message(paste("Run", i, "complete."))
@@ -95,7 +106,8 @@ rangeSVM <- function(xy1, xy2, sdm = NULL, nrep = 100, weight = FALSE) {
   params_best_df <- do.call(rbind, params_best)
   params_best_df$params <- paste0(params_best_df$gamma, params_best_df$cost)
   mostFreq <- names(which(table(params_best_df$params) == max(table(params_best_df$params))))
-  params_best_df_mostFreq <- params_best_df[params_best_df$params == mostFreq,1:2]
+  params_best_df_mostFreq <- params_best_df[params_best_df$params == mostFreq, 1:2]
+  
   # run final model
   m <- e1071::svm(sp ~ ., data = xy, gamma = params_best_df_mostFreq$gamma[1], 
                   cost = params_best_df_mostFreq$cost[1], class.weights = cw)
@@ -103,15 +115,14 @@ rangeSVM <- function(xy1, xy2, sdm = NULL, nrep = 100, weight = FALSE) {
   return(m)
 }
 
-###############################################################################
-
 #' Generate binary raster based on predictions of SVM model.
 #'
-#' \code{rasterizeRangeSVM()} returns a binary raster representing the ranges of the two species
+#' \code{rangeSVM_predict()} returns a binary raster representing the ranges of the two species
 #' predicted by the fitted SVM tuned with \code{rangeSVM()}.
 #'
 #' @param svm Model object for the SVM, returned by \code{rangeSVM()}.
-#' @param r Raster with the extent desired for the prediction. The values do not matter.
+#' @param r Raster with the extent desired for the prediction. The values for cells used for predictions must 
+#' have non-NA values, but the particular values do not matter.
 #' @param sdm Raster or RasterStack representing environmental suitability (can be predictions from SDMs).
 #' These rasters must match the predictor variables used in the SVM. Default is NULL.
 #' @return The binary Raster representing the SVM predictions.
@@ -120,20 +131,20 @@ rangeSVM <- function(xy1, xy2, sdm = NULL, nrep = 100, weight = FALSE) {
 #' 
 #' @examples
 #' \dontrun{
-#' # raster prediction for spatial SVM
-#' svm.sp <- rangeSVM(xy1, xy2)
-#' svm.sp.r <- rasterizeRangeSVM(svm.sp, r)
-#' plot(svm.sp.r)
+#' # raster prediction for xy SVM (spatial)
+#' svm.xy <- rangeSVM(xy1, xy2)
+#' svm.xy.r <- rangeSVM_predict(svm.xy, r)
+#' plot(svm.xy.r)
 #' 
-#' # raster prediction for hybrid SVM
-#' svm.hyb <- rangeSVM(xy1, xy2, raster::stack(sdm1, sdm2))
-#' svm.hyb.r <- rasterizeRangeSVM(svm.hyb, r, raster::stack(sdm1, sdm2))
-#' plot(svm.hyb.r)
+#' # raster prediction for xyEnv SVM (spatial + SDM predictions)
+#' svm.xyEnv <- rangeSVM(xy1, xy2, raster::stack(sdm1, sdm2))
+#' svm.xyEnv.r <- rangeSVM_predict(svm.xyEnv, r, raster::stack(sdm1, sdm2))
+#' plot(svm.xyEnv.r)
 #' }
 #' @export
 
-rasterizeRangeSVM <- function(svm, r, sdm = NULL) {
-  # extract coordinates from shared extent
+rangeSVM_predict <- function(svm, r, sdm = NULL) {
+  # extract centroid coordinates from shared extent raster cells
   r.pts <- raster::rasterToPoints(r, spatial = TRUE)
   r.xy <- sp::coordinates(r.pts)
   # if sdm prediction raster input, add the values to the matrix 
