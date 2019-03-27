0134e7e397f5442abc23eb9968e5ee6d877965d8
Cory Merow <cory.merow@gmail.com>
2019-03-27 10:20:21 -0400

    made data dir

---
+++ maskRangeR/R/annotate.R
@@ -0,0 +1,65 @@
+#==================================================================================
+#==================================================================================
+#' @title Annotate point data with rasters based on matching dates.
+#'
+#' @description Annotate point with rasters based on matching dates associated with points to dates associated with raster. Specifically, we're thinking of the points as species observations and the rasters as remotely sensed environmental layers, but they can represent any points and rasters with dates.
+#' @details
+#' See Examples.
+#' @param datedOccs a `SpatialPointsDataFrame` where one column is labeled `date` and has class `POSIXct`, e.g., as obtained from using `lubridate::parse_date_time` 
+#' @param env a raster stack
+#' @param envDates a vector of dates the same length as `env`. The vector should have class `POSIXct`, e.g., as obtained from using `lubridate::parse_date_time`
+#' @param dateScale string: 'year', 'month', or 'day'
+# @examples
+#'
+#'
+# @return
+#' @author Cory Merow <cory.merow@@gmail.com>,
+#' @note To apply multiple masks, e.g., elevation and forest cover, use separate calls to maskRS.
+# @seealso
+# @references
+# @aliases - a list of additional topic names that will be mapped to
+# this documentation when the user looks them up from the command
+# line.
+# @family - a family name. All functions that have the same family tag will be linked in the documentation.
+#' @export
+
+
+# try just matching dates between envDates and the occurrence dates
+annotate=function(datedOccs,
+                  env,
+                  envDates,
+                  dateScale){
+  # uses the convention that dates for values indicated by a year are YYYY-01-01
+  # uses the convention that dates for values indicated by a month are YYYY-MM-01
+  if(dateScale=='year') {
+    form="%Y"
+  } else if (dateScale=='month'){
+    form="%Y-%m"
+  } else if (dateScale=='day'){
+    form="%Y-%m-%d"
+  } else { stop('Choose a supported value for dateScale: year, month or day')}
+  
+  datedOccs$myDate=format(datedOccs$date,form)
+  uniqueDates=stats::na.omit(unique(datedOccs$myDate))
+  myEnvDates=format(envDates,form)
+
+  out=lapply(seq_along(uniqueDates),function(x,datedOccs,myEnvDates,env){
+    pts=datedOccs[which(datedOccs$myDate==uniqueDates[x]),]
+    keep=match(uniqueDates[x],myEnvDates)
+    if(is.na(keep)) {
+      print(paste0('Environmental layers were missing for date ',uniqueDates[x]) )
+      pts$env=rep(NA,nrow(pts))
+      return(pts)
+    }
+    if(length(keep)>1) stop('Multiple dates in your environmental layers correspond 
+                            to the dates for your occurrences; make sure your 
+                            environmental layers have unique dates')
+    pts$env=raster::extract(env[[keep]],pts)
+    return(pts)
+  },datedOccs=datedOccs,myEnvDates=myEnvDates,env=env)
+
+  tmp=suppressWarnings(do.call('rbind',out))
+  lost=nrow(datedOccs)-nrow(tmp)
+  if(lost>0) print(paste(lost,'points were omitted because they had no dates'))
+  return(tmp)
+}
---
+++ maskRangeR/R/svm.R
@@ -0,0 +1,227 @@
+#' Classify species ranges based on occurrence coordinates and SDM scores.
+#'
+#' \code{rangeSVM()} returns a tuned support vector machine (SVM) model that predicts 
+#' species identity based on predictors that are solely spatial, based on occurrence coordinates,
+#' or a combination of spatial and environmental, based on both occurrence coordinates and 
+#' environmental suitability values. Suitability values can be predicted with species distribution
+#' models (SDMs; a.k.a. ecological niche models).
+#'
+#' @param xy1 Matrix or data frame of occurrence coordinates for species 1.
+#' @param xy2 Matrix or data frame of occurrence coordinates for species 2.
+#' @param ... Other matrices or data frames of occurrence coordinates for additional species.
+#' @param sdm Raster or RasterStack representing environmental suitability (can be predictions from SDMs). 
+#' These must have the same extent as both species' occurrence points. Default is NULL.
+#' @param nrep Numeric for number of SVM tuning iterations. Default is 100.
+#' @param weight Boolean. If TRUE, species with fewer occurrence records are weighted higher in the SVM.
+#' Default is FALSE.
+#' @return The tuned SVM model.
+#' @details The tuning operation uses \code{tune.svm()} from the e1071 package, which performs 10-fold
+#' cross validation and selects the best model based on classification error. Ranges of the cost and gamma
+#' parameters are explored in the tuning exercise. The tuning function is iterated \code{nrep} times, and the
+#' parameter combination used most frequently across all iterations is used to build a final SVM model.
+#' 
+#' When \code{sdm = NULL}, the SVM is purely spatial, based only on the occurrence coordinates of
+#' each species. Otherwise, the SVM is fit with both a spatial predictor and any additional ones added as
+#' rasters. These extra predictors can be based on predictions from a species distribution model 
+#' (SDM; a.k.a. ecological niche model), and in this case would represent environmental or climatic
+#' suitability, depending on the variables used in the SDM.
+#' 
+#' 
+#' @examples
+#' \dontrun{
+#' ########## Maksing by biotic interactions
+#' simulateData()
+#' # Spatial SVMs
+#' svm.SP <- rangeSVM(sp1.xy, sp2.xy, sp3.xy, nrep=10)
+#' # Use SVM to create a raster of predicted regions
+#' rand_svm.SP <- rangeSVM_predict(svm = svm.SP, r = r1.svm)
+#' # Plot the results
+#' plot(rand_svm.SP, col=c("yellow","pink","lightblue"))
+#' points(sp1.xy, pch = 20, cex = 0.75, col = "orange")
+#' points(sp2.xy, pch = 20, cex = 0.75, col = "green")
+#' points(sp3.xy, pch = 20, cex = 0.75, col = "blue")
+#' # We can use this SVM as a mask over our original SDM predictions.
+#' # masked SDM predictions for variegatus
+#' sp1_svm.SP <- rand_svm.SP == 1
+#' sp1_svm.SP[sp1_svm.SP == 0] <- NA
+#' sp1_svmSP_mask <- mask(r1.sdm, sp1_svm.SP)
+#' # masked SDM predictions for tridactylus
+#' sp2_svm.SP <- rand_svm.SP == 2
+#' sp2_svm.SP[sp2_svm.SP == 0] <- NA
+#' sp2_svmSP_mask <- mask(r2.sdm, sp2_svm.SP)
+#' # masked SDM predictions for torquatus
+#' sp3_svm.SP <- rand_svm.SP == 3
+#' sp3_svm.SP[sp3_svm.SP == 0] <- NA
+#' sp3_svmSP_mask <- mask(r3.sdm, sp3_svm.SP)
+#' ##Plot the predicted realized distributions for each species.
+#' # Create 3-panel figure
+#' par(mfrow = c(1,3))
+#' plot(sp1_svmSP_mask)
+#' plot(sp2_svmSP_mask)
+#' plot(sp3_svmSP_mask)
+#' ## Hybrid SVMs
+#' # Create SVM
+#' svmHYB <- rangeSVM(sp1.xy, sp2.xy, sp3.xy, sdm = raster::stack(r1.sdm, r2.sdm, r3.sdm), nrep = 10)
+#' # Use SVM to create a raster of predicted regions
+#' rand_svmHYB <- rangeSVM_predict(svm = svmHYB, r = r1.sdm, sdm = raster::stack(r1.sdm, r2.sdm, r3.sdm))
+#' ## Plot the SVM results.
+#' plot(rand_svmHYB, col=c("yellow","pink","lightblue"))
+#' points(sp1.xy, pch = 20, cex = 0.75, col = "orange")
+#' points(sp2.xy, pch = 20, cex = 0.75, col = "green")
+#' points(sp3.xy, pch = 20, cex = 0.75, col = "blue")
+#' ## Use the hybrid SVM as a mask over our SDM predictions.
+#' # masked SDM predictions for variegatus
+#' sp1_svmHYB <- rand_svmHYB == 1
+#' sp1_svmHYB[rand_svmHYB == 0] <- NA
+#' sp1_svmHYB_mask <- mask(r1.sdm, sp1_svmHYB)
+#' # masked SDM predictions for tridactylus
+#' sp2_svmHYB <- rand_svmHYB == 2
+#' sp2_svmHYB[sp2_svmHYB == 0] <- NA
+#' sp2_svmHYB_mask <- mask(r2.sdm, sp2_svmHYB)
+#' # masked SDM predictions for torquatus
+#' sp3_svmHYB <- rand_svmHYB == 3
+#' sp3_svmHYB[sp3_svmHYB == 0] <- NA
+#' sp3_svmHYB_mask <- mask(r3.sdm, sp3_svmHYB)
+#' ## Plot the predicted realized distributions for each species.
+#' # Create 3-panel figure
+#' par(mfrow = c(1,3))
+#' plot(sp1_svmHYB_mask)
+#' plot(sp2_svmHYB_mask)
+#' plot(sp3_svmHYB_mask)
+#' }
+#' @export
+#' 
+#' 
+
+rangeSVM <- function(xy1, xy2, ..., sdm = NULL, nrep = 100, weight = FALSE) {
+  
+  # bind both coordinate matrices
+  xy <- as.data.frame(rbind(xy1, xy2))
+  otherSp <- list(...)
+  nsp <- 2 + length(otherSp)
+  sp.num <- c(nrow(xy1), nrow(xy2), sapply(otherSp, nrow))
+  xy.otherSp <- do.call("rbind", otherSp)
+  xy <- rbind(xy, xy.otherSp)
+  # make sure the column names are x and y
+  names(xy) <- c("x", "y")
+  
+  # if using sdm prediction rasters as extra predictor variable, add the values to the matrix 
+  if(!is.null(sdm)) {
+    sdm.vals <- raster::extract(sdm, xy)
+    xy <- cbind(xy, sdm = sdm.vals)
+  }
+  # add a species signifier field
+  xy$sp <- factor(rep(1:nsp, times=sp.num))
+  
+  # define class weights
+  if(weight == TRUE) {
+    cw <- 1/table(xy$sp)
+  } else {
+    cw <- table(xy$sp)/table(xy$sp)
+  }
+  
+  # define a range for parameters C and gamma, which control complexity of fit
+  # ranges based on suggestions from https://www.csie.ntu.edu.tw/~cjlin/papers/guide/guide.pdf
+  C_range <- 2^seq(-5, 15, 2)
+  gamma_range <- 2^seq(-15, 3, 2)
+  
+  # perform 10-fold cross validation for ranges of C and gamma
+  # methodology based on:
+  # @article{karatzoglou2005support,
+  # title={Support vector machines in R},
+  # author={Karatzoglou, Alexandros and Meyer, David and Hornik, Kurt},
+  # year={2005},
+  # publisher={Department of Statistics and Mathematics, WU Vienna University of Economics and Business}
+  params_best <- list()
+  performance_best <- list()
+  
+  for(i in 1:nrep) {
+    m.tune <- e1071::tune.svm(sp ~ ., data = xy, gamma = gamma_range, cost = C_range, class.weights = cw)  
+    # get optimal parameter values
+    params_best[[i]] <- m.tune$best.parameters
+    performance_best[[i]] <- m.tune$best.performance
+    message(paste("Run", i, "complete."))
+  }
+  # # function to extract the value of either parameter that was best most often
+  performance_best <- do.call(rbind, performance_best)
+  params_best_df <- cbind(do.call(rbind, params_best)[,1:2], performance_best)
+  params_best_count <- dplyr::count(params_best_df, gamma, cost)
+  
+  # Add error message if there is no most frequent combination of parameters
+  if(sum(params_best_count$n > 1) == 0){
+    stop("Tuning did not produce a most frequent combination of SVM parameters. Please increase nrep and try again.")
+  }
+  
+  params_mostFreq <- params_best_count[params_best_count$n == max(params_best_count$n),1:2]
+  
+  if(nrow(params_mostFreq) > 1){
+    mean_performance <- numeric(nrow(params_mostFreq))
+    for (i in 1:nrow(params_mostFreq)) {
+      gamma_i <- as.numeric(params_mostFreq[i, 1])
+      cost_i <- as.numeric(params_mostFreq[i, 2])
+      mostFreq_i <- params_best_df[params_best_df$gamma == gamma_i & params_best_df$cost == cost_i, ]
+      mean_performance[i] <- mean(mostFreq_i$performance_best)
+    }
+    param_combo_best <- params_mostFreq[which.min(mean_performance), ]
+  } else {
+    param_combo_best <- params_mostFreq
+  }
+  
+  # run final model
+  m <- e1071::svm(sp ~ ., data = xy, gamma = param_combo_best$gamma, 
+                    cost = param_combo_best$cost, class.weights = cw)
+  
+  return(m)
+}
+
+#' Generate a raster based on predictions of SVM model with values corresponding to the species.
+#'
+#' \code{rangeSVM_predict()} returns a raster representing the ranges of the species
+#' predicted by the fitted SVM tuned with \code{rangeSVM()}.
+#'
+#' @param svm Model object for the SVM, returned by \code{rangeSVM()}.
+#' @param r Raster with the extent desired for the prediction. The values for cells used for predictions must 
+#' have non-NA values, but the particular values do not matter.
+#' @param sdm Raster or RasterStack representing environmental suitability (can be predictions from SDMs).
+#' These rasters must match the predictor variables used in the SVM. Default is NULL.
+#' @return The  Raster representing the SVM predictions.
+#' @details The values of the output raster are 1, 2, ..., corresponding to xy1, xy2, and any additional species used in \code{rangeSVM()}.
+#' These values represent the identities of the species.
+#' 
+#' @examples
+#' \dontrun{
+#' # raster prediction for xy SVM (spatial)
+#' svm.xy <- rangeSVM(xy1, xy2)
+#' svm.xy.r <- rangeSVM_predict(svm.xy, r)
+#' plot(svm.xy.r)
+#' 
+#' # raster prediction for xyEnv SVM (spatial + SDM predictions)
+#' svm.xyEnv <- rangeSVM(xy1, xy2, raster::stack(sdm1, sdm2))
+#' svm.xyEnv.r <- rangeSVM_predict(svm.xyEnv, r, raster::stack(sdm1, sdm2))
+#' plot(svm.xyEnv.r)
+#' }
+#' @export
+
+rangeSVM_predict <- function(svm, r, sdm = NULL) {
+  # extract centroid coordinates from shared extent raster cells
+  r.pts <- raster::rasterToPoints(r, spatial = TRUE)
+  r.xy <- sp::coordinates(r.pts)
+  # rename column names to match response of svm
+  colnames(r.xy) <- c("x", "y")
+  # if sdm prediction raster input, add the values to the matrix 
+  if(!is.null(sdm)) {
+    sdm.vals <- raster::extract(sdm, r.xy)
+    r.xy <- cbind(r.xy, sdm = sdm.vals)
+    sdm.names <- seq(3, 2+ncol(sdm.vals))
+    colnames(r.xy)[sdm.names] <- names(svm$x.scale$`scaled:center`[sdm.names])
+  }
+  
+  # predict species identity of all coordinates with svm
+  sp12.svm <- predict(svm, r.xy)
+  # convert factor response to integer
+  sp12.svm <- as.numeric(as.character(sp12.svm))
+  # convert back to raster
+  r.pts$pred <- sp12.svm
+  sp12.svm.ras <- raster::rasterize(r.pts, r, "pred")
+  return(sp12.svm.ras)
+}
