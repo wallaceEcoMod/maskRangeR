8c99bedebb3d0d4171eb0deeefdf5e0d2aa872a7
CORY MEROW <ctg@MacBook-Pro-2.local>
2019-10-12 18:45:26 -0400

    merge

---
+++ maskRangeR/R/manyMaskSensitivity.R
@@ -0,0 +1,75 @@
+#===========================================================================
+#===========================================================================
+#' @title Sensitivity testing for masks
+#'
+#' @description Compare how masks of climate tolerances affect predicted area
+#' @details
+#' See Examples.
+
+#' @param crt A raster stack; the output from the function maskRangeR::cropResampleTrim
+#' @param rasProj (optional) a character string: a proj4string showing the projection of the environmental layers. If NULL, areas will be estimated using the raster package.
+#' @param maskBounds A data.frame with columns indicating the layer name (matching the names in maskStack), and the min and max values of that layer to be used for masking.
+#' 
+#' @return returns a data.frame where row names are the environmental layer name combinations, and Area is expressed in square km, unless a projection is supplied
+#'  
+#' @author Peter Galante <pgalante@@amnh.org>
+#' @export
+#' @importFrom grDevices dev.new rainbow
+#' @importFrom stats na.omit quantile  setNames
+#' @importFrom utils combn 
+
+manyMaskSensitivity <- function(crt, rasProj = NULL, maskBounds = NULL){
+  ## define function for calculating areas
+  calcAreas <- function(r2){
+    tapply(raster::area(r2), r2[], sum)
+  }
+  ### create combinations of mask layers at list of matrices
+  masks <- crt$maskStack
+  x <- names(masks)
+  m <- 1:(raster::nlayers(masks)-1)
+  combinos <- lapply(m, function(r){utils::combn(x,r)})
+  ### Create a list of names of mask layers as list of lists and subset maskList by each sublist
+  combiNamesList <- lapply(combinos, function(x) lapply(apply(x, 2, list), unlist))
+  combiRastList <- unlist(lapply(combiNamesList, function(y) lapply(y, function(x) raster::subset(masks, x))))
+  ### Get many outputs; each combination
+  manyOuts <- lapply(combiRastList, function(x) lotsOfMasks(expertRaster, 
+                                                            maskStack = x, 
+                                                            maskBounds = maskBounds))
+  if (is.null(rasProj)){
+    ### Calculate areas for each "refinedDist"
+    manyRefined <- lapply(manyOuts, function(x) x$refinedDist)
+    manyMasksArea <- stats::setNames(as.data.frame(cbind(unlist(lapply(manyRefined, 
+                                                         function(r2){tapply(area(r2), r2[], sum)})), 
+                                                         lapply(combiRastList, names)), 
+                                                   row.names = lapply(combiRastList, names)),  
+                                     c("Area", "Layer"))
+    
+    v1 <- manyMasksArea[with(manyMasksArea, order(unlist(manyMasksArea$Area))), ]
+  } 
+  if (!is.null(rasProj)){
+    proj.FUN<-function(rasterList, proj4){
+      raster::crs(rasterList) <- proj4
+    }
+    ### Reproject
+    lapply(manyOuts, proj.FUN, rasProj)
+    ### Calculate areas for each "refinedDist"
+    manyRefined <- lapply(manyOuts, function(x) x$refinedDist)
+    manyMasksArea <- stats::setNames(as.data.frame(cbind(unlist(lapply(manyRefined, calcAreas)), lapply(combiRastList, names)), row.names = lapply(combiRastList, names)),  c("Area", "Layer"))
+    v1 <- manyMasksArea[with(manyMasksArea, order(unlist(manyMasksArea$Area))), ]
+  }
+  v1 <- manyMasksArea[with(manyMasksArea, order(unlist(manyMasksArea$Area))), ]
+  ## Plotting function
+  .manyMaskSensitivityPlots <- function(v1){
+    graphics::barplot(height = unlist(v1$Area), space = 1, xlab = "", names.arg = "")
+    graphics::text(seq(1.5, (0.5 + nrow(v1) + nrow(v1)-1), 2), graphics::par("usr")[3]-.25, srt = 60, adj=1, xpd=T, labels = cbind(unlist(lapply(v1$Layer, toString))), cex= 0.65)
+  }
+  .manyMaskSensitivityPlots(v1)
+  return(v1)
+}
+
+## Plotting function
+#' @importFrom graphics barplot legend par plot points text
+.manyMaskSensitivityPlots <- function(v1){
+  graphics::barplot(height = unlist(v1$Area), space = 1, xlab = "", names.arg = "")
+  graphics::text(seq(1.5, (0.5 + nrow(v1) + nrow(v1)-1), 2), par("usr")[3]-.25, srt = 60, adj=1, xpd=T, labels = cbind(unlist(lapply(v1$Layer, toString))), cex= 0.65)
+}
---
+++ maskRangeR/R/thresholdSensitivity.R
@@ -0,0 +1,114 @@
+#===========================================================================
+#===========================================================================
+#' @title Sensitivity testing for thresholds
+#'
+#' @description Measure a number of reasonable thresholds and calculate areas based on these thresholds
+#' @details
+#' See Examples.
+
+#' @param datedOccs a `SpatialPointsDataFrame` where one column is labeled `date` and has class `POSIXct`, e.g., as obtained from using `lubridate::parse_date_time` 
+#' @param maskLayer the layer from which you will build the mask. Usually the most recent satellite derived raster
+#' @param maskClass either top5, aroundSelected, quants, or userSpecified. "top5" ranks the environmental values at occurrences and returns the 5 highest observed values."aroundSelected" selects two observed values above and two below a user-specified threshold value. "quants" returns quantile values as thresholds. "userSpecified" will take a list of values defined by the user.
+#' @param sdm previously generated species distribution model
+#' @param maskProjection (optional) a proj4string showing the projection of the maskLayer. If NULL, areas will be estimated using the raster package.
+#' @param maskVal (optional) a user defined value for thresholding when using the "aroundSelected" maskClass, or if "userSpecified", a list of thresholds to use.
+#' @param selectedValue  (optional) a user selected value around which masks will be selected using the nearest two threshold on either side
+#' 
+#' @return returns a list containing two items. The first is a rasterstack of the masked distributions. The second item is a table of thresholds and areas
+#'  
+#' @author Peter Galante <pgalante@@amnh.org>
+#' @export
+
+## add swmap crab, olinguito
+########################
+## testing
+#test <- thresholdSensitivity(datedOccs, maskLayer, maskClass = "userSpecified", sdm=sdm, 
+#                         maskProjection = "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6378140 +b=6356750 +units=m +no_defs", 
+#                         maskVal = seq(0, 100, length.out =500))
+########################
+
+thresholdSensitivity <- function(datedOccs, maskLayer, maskProjection = NULL, maskClass, maskVal = NULL, sdm, selectedValue=NULL){
+  ## Maskthresholds
+  .maskThresh <- function(datedOccs, maskClass, selectedValue = NULL, maskVal = NULL){
+    if (maskClass == "top5"){
+      ### multiple sensible masks
+      # Top 5 thresholds
+      maskThresh <- rev(sort(unique(stats::na.omit(datedOccs$env))))[1:5]
+    } else if (maskClass == "aroundSelected"){
+      # buffer around selected value
+      aroundSelected <- function(datedOccs, selectedValue){
+        maskVals <- rev(sort(unique(stats::na.omit(datedOccs$env))))[(which.min(abs(rev(sort(unique(stats::na.omit(datedOccs$env)))) - selectedValue)) - 1) : (which.min(abs(rev(sort(unique(stats::na.omit(datedOccs$env)))) - selectedValue)) + 2)]
+        return(maskVals)
+      }
+      maskThresh <- aroundSelected(datedOccs, selectedValue = maskVal) 
+    } else if (maskClass == "quants"){
+      # Quantile values
+      maskThresh <- as.numeric(raster::quantile(datedOccs$env, na.rm=T))
+    } else if (maskClass == "userSpecified"){
+      # user defined
+      maskThresh <- maskVal
+      return(maskThresh)
+    }
+  }
+  maskThresh <- .maskThresh(datedOccs, maskClass, selectedValue, maskVal)
+  ## Bounded omission/commission
+  
+  ### makes several logicStrings
+  # get most recent forest cover
+  stringsOfLogic <- paste0('maskLayers<', maskThresh)
+  
+  ### create a list of rasters
+  sensitivityStack <- lapply(stringsOfLogic, function(x) maskRanger(initialDist=sdm, maskLayers=maskLayer, logicString=x)$refinedDist)
+  ### Set projection if available
+  proj.FUN<-function(rasterList, proj4){
+    raster::crs(rasterList) <- proj4
+  }
+  if (!is.null(maskProjection)){
+    lapply(sensitivityStack, proj.FUN, maskProjection)
+  }
+  ## Calculate areas for each refinedDist
+  # Set all values in each reginedDist = 1, calculate area of 1's
+  ras.FUN <- function(r1){
+    r1[!is.na(r1)] <- 1
+    return(r1)
+  }
+  # change all values !=na to 1
+  sensitivityOnes <- lapply(sensitivityStack, ras.FUN)
+  # fcn to calculate area based on 1's
+  calcAreas <- function(r2){
+    tapply(raster::area(r2), r2[], sum)
+  }
+  # apply area calculation to list of rasters. results are square km
+  sensitivityAreas <- lapply(sensitivityOnes, calcAreas)
+  ## Plotting metrics
+  .sensitivityPlotting <- function(stringsOfLogic, sensitivityStack, sensitivityAreas){
+    ## Plotting each mask and one plot of area vs threshold
+    # area vs. threshold
+    maskValues <- gsub(".*<","",stringsOfLogic)
+    names(sensitivityStack) <- paste0("threshold_of_", maskValues)
+    sensitivityStack<-raster::stack(sensitivityStack)
+    grDevices::dev.new()
+    graphics::par(mfrow=c(2,(length(stringsOfLogic)/2)+1))
+    colPal <- grDevices::rainbow(5)
+    lapply(names(sensitivityStack), function(x) graphics::plot(sensitivityStack[[x]], main = x, xlab = "long", ylab = "lat"))
+    #### for here allow for all threshold values, but only plot some maps
+    
+    graphics::plot(maskValues, sensitivityAreas, ylab = "Area (square km)", xlab = "Mask values", main = "Mask Threshold Area Sensitivity", col = colPal, pch = 19, type= "l")
+    graphics::points(maskValues, sensitivityAreas, ylab = "Area (square km)", xlab = "Mask values", main = "Mask Threshold Area Sensitivity", col = colPal, pch = 19)
+    # Get values to add to plot
+    nums <- gsub(".*= ", "", sensitivityAreas)
+    nums <- gsub( ").*$", "", nums)
+    nums <- format(round(unlist(lapply(nums, function(x) as.numeric(as.character(x)))), 3))
+    # add values
+    graphics::text(maskValues, sensitivityAreas, labels = nums, cex= 1, pos=3)
+    # add legend
+    graphics::legend(x = "topleft", col = colPal, legend = names(sensitivityStack), pch = 19, cex=0.7)
+    print(cbind(maskValues, sensitivityAreas))
+    sensitivityReturns <- NULL
+    return(list(sensitivityStack, cbind(maskValues, sensitivityAreas)))
+  }
+  .sensitivityPlotting(stringsOfLogic, sensitivityStack, sensitivityAreas)
+}
+
+
+
