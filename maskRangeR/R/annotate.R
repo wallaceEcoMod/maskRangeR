72f14f18608c2db8a185d2e7d375a27153227b78
Pgalante <pgalante@amnh.org>
2021-03-15 14:55:46 -0400

    vignette update and annotate help update

--- maskRangeR/R/annotate.R
+++ maskRangeR/R/annotate.R
@@ -5,7 +5,7 @@
 #' @description Annotate point data with rasters based on matching dates associated with points to dates associated with rasters. Specifically, we're thinking of the points as species observations and the rasters as remotely sensed environmental layers, but they can represent any points and rasters with dates.
 #' @details
 #' See Examples.
-#' @param datedOccs a `SpatialPointsDataFrame` where one column is labeled `date` and has class `POSIXct`, e.g., as obtained from using `lubridate::parse_date_time` 
+#' @param datedOccs a `SpatialPointsDataFrame` of occurrence localities (generally longitude and latitude in decimal degrees) paired with dates. One column must be labeled `date` and have class `POSIXct`, e.g., as obtained from using `lubridate::parse_date_time` 
 #' @param env a raster stack
 #' @param envDates a vector of dates the same length as `env`. The vector should have class `POSIXct`, e.g., as obtained from using `lubridate::parse_date_time`
 #' @param dateScale string: 'year', 'month', or 'day'
